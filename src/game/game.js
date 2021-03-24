befe3e60cd4fe2b3d0a8e0845852d99681d5f3d5
Zack Jameson <zacharymjameson@gmail.com>
2021-03-23 20:22:15 -0400

    update readme

--- src/game/game.js
+++ src/game/game.js
@@ -4,8 +4,8 @@ export default function Game(props) {
   return (
     <div className="game">
       <h2>{props.App}</h2>
-      <div className="game_contentrating">by {props["Content Rating"]}</div>
-      <div className="game_price">{props.Price}</div>
+      <div className="game_contentrating">Content Rating: {props["Content Rating"]}</div>
+      <div className="game_price">Price: {props.Type}, ${props.Price}</div>
       <div className="game_rating">Rating: {props.Rating}</div>
       <div className="game_size">File Size: {props.Size}</div>
     </div>
